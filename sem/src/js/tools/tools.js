d387c7c8bc09c21e3e82570c3e61d280f323f7d7
patulak <patulakkolac@gmail.com>
2026-04-30 09:51:04 +0200

    working on menu

--- sem/src/js/tools/tools.js
+++ sem/src/js/tools/tools.js
@@ -1,6 +1,11 @@
 export function show_view(view_name) {
   document.querySelectorAll("main > section").forEach(section => {
-    section.style.display = section.id !== `view-${view_name}` ? "none" : "flex";
+    if (section.classList.contains("show-view")) {
+      section.classList.remove("show-view");
+    }
+    if (section.id == `view-${view_name}`) {
+      section.classList.add("show-view");
+    }
   });
   //app_state.current_view = view_name;
 }
